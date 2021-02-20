e4a4b5da794b9909372ad847326cb9dee5aefc84
stevenkl <stevenkl@rbx.run>
2021-02-20 21:15:55 +0100

    Added basic validation fon Config state

--- config_validations.go
+++ config_validations.go
@@ -1,18 +1,38 @@
 package main
 
+import (
+	"errors"
+	"os"
+)
 
 func validateServerConfig(c *Config) error {
+	if c.Server.Host == "" {
+		return errors.New("server.host field must not an empty string")
+	}
+	if c.Server.Port == 0 {
+		return errors.New("server.port field must greater than 0")
+	}
 	return nil
 }
 
 func validateStorageConfig(c *Config) error {
+	_, err := os.Stat(c.Storage.Path)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func validateUsersConfig(c *Config) error {
+	if len(c.Users.Users) <= 0 {
+		return errors.New("it must at least one user be defined")
+	}
 	return nil
 }
 
 func validateJobsConfig(c *Config) error {
+	if len(c.Jobs.Jobs) <= 0 {
+		return errors.New("it must at least one job be defined")
+	}
 	return nil
 }
